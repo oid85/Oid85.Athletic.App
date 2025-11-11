1f7e6a39561b9f579198070edaab1083265972b6
oid85 <oid85@mail.ru>
2025-11-12 00:06:56 +0700

    .

--- oid85-athletic-app/src/component/Screens/MainScreen.js
+++ oid85-athletic-app/src/component/Screens/MainScreen.js
@@ -2,10 +2,11 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import 'react-tabs/style/react-tabs.css'
 import { Alert } from '../Alert/Alert'
-import {ExerciseTemplateList} from "./ExerciseTemplateList"
-import {Training} from "./Training"
-import {TrainingList} from "./TrainingList"
-import {Plan} from "./Plan"
+import {ExerciseTemplateList} from "../ExerciseTemplateList/ExerciseTemplateList"
+import {Training} from "../Training/Training"
+import {TrainingList} from "../TrainingList/TrainingList"
+import {Plan} from "../Plan/Plan"
+import './styles.css'
 
 export const MainScreen = () => {
     const alert = useSelector(state => state.app.alert)
@@ -14,7 +15,7 @@ export const MainScreen = () => {
         <React.Fragment>            
             {alert && <Alert text={alert} />}
             <div>
-                <div style={{ flex: 1, display: 'flex', flexDirection: "row" }}>
+                <div className='horizontal-container'>
                     <ExerciseTemplateList />
                     <Training />
                     <TrainingList />
