c4ddf46f5b37ebb7966e1777b88a3eb8b065caa4
oid85 <oid85@mail.ru>
2025-11-20 20:23:24 +0700

    .

--- oid85-athletic-app/src/component/PlanList/PlanListItemTraining.js
+++ oid85-athletic-app/src/component/PlanList/PlanListItemTraining.js
@@ -3,13 +3,21 @@ import 'bootstrap/dist/css/bootstrap.css'
 import './styles.css'
 
 export const PlanListItemTraining = ({training}) => {
+
+    let className = 'plan-list-item-training'
+
+    if (!training.name) { className = className + ' plan-list-item-training-lightgreen' }    
+    if (training.totalWeight >= 0 && training.totalWeight < 500) { className = className + ' plan-list-item-training-lightyellow' }
+    if (training.totalWeight >= 500 && training.totalWeight < 2000) { className = className + ' plan-list-item-training-lightsalmon' }
+    if (training.totalWeight >= 2000) { className = className + ' plan-list-item-training-tomato' }
+
     return (
         <React.Fragment>
-            <div className='plan-list-item-training'>
+            <div className={className}>
                 {
                     !training.name ? <div></div> :
                     <div>
-                        <div  className='plan-list-item-training-name'>{training.name}</div>
+                        <div className='plan-list-item-training-name'>{training.name}</div>
                         <div>{training.totalCountIterations} повт.</div>
                         <div>{training.totalWeight} кг</div>                        
                     </div>
