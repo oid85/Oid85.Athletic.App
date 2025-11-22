cb3aba7ab9a6a70ac7c703c8ab9c8525d2dd9197
oid85 <oid85@mail.ru>
2025-11-22 22:35:28 +0700

    .

--- oid85-athletic-app/src/component/PlanList/PlanListItem.js
+++ oid85-athletic-app/src/component/PlanList/PlanListItem.js
@@ -1,13 +1,23 @@
 import React from 'react'
+import moment from 'moment'
 import 'bootstrap/dist/css/bootstrap.css'
 import './styles.css'
 import { PlanListItemCalendar } from './PlanListItemCalendar'
 import { PlanListItemTraining } from './PlanListItemTraining'
 
 export const PlanListItem = ({key, planTraining}) => {
+
+    let className = 'plan-list-item'
+    let today = moment().format('YYYY-MM-DD').toString()
+    
+    if (planTraining.date == today)
+    {
+        className = className + ' plan-list-item-today'
+    }
+
     return (
         <React.Fragment>
-            <div className='plan-list-item' key={key}>
+            <div className={className} key={key}>
                 <PlanListItemCalendar date = {planTraining.date} />
                 <PlanListItemTraining training = {{...planTraining.morningTraining}} planTraining = {planTraining} />
                 <PlanListItemTraining training = {{...planTraining.dayTraining}} planTraining = {planTraining} />
--- oid85-athletic-app/src/component/PlanList/PlanListItemCalendar.js
+++ oid85-athletic-app/src/component/PlanList/PlanListItemCalendar.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import {Calendar} from '../Calendar/Calendar'
+import moment from 'moment'
 import 'bootstrap/dist/css/bootstrap.css'
 import './styles.css'
 
 export const PlanListItemCalendar = ({date}) => {    
+
+    let className = 'plan-list-item-calendar'
+    let today = moment().format('YYYY-MM-DD').toString()
+    
+    if (date == today)
+    {
+        className = className + ' plan-list-item-calendar-today'
+    }    
+
     return (
         <React.Fragment>
-            <div className='plan-list-item-calendar'>
+            <div className={className}>
                 <Calendar date = {date} />
             </div>
         </React.Fragment>
